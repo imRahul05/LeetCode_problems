e0a6a5a698f2e3ab302d68a177cc5c9e0a084d35
Rahul kumar <118787765+imRahul05@users.noreply.github.com>
2024-04-25 10:55:04 +0530

    Create HelloWorld.js

---
+++ HelloWorld.js
@@ -0,0 +1,22 @@
+var createHelloWorld = () => "Hello World"
+console.log(createHelloWorld())
+
+or
+
+var createHelloWorld = function() {
+  return =>"HelloWorld"
+  
+console.log(createHelloWorld())
+
+or 
+
+
+---------Leetcode-----------------
+
+  /**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return () =>"Hello World";
+};
