1aa0feafbaed36dee98a83524e8ac54f6217fba3
imrahul05 <rahulkumar20000516@gmail.com>
2024-10-20 20:30:07 +0530

    Refactor removeElement function to improve code readability and efficiency

---
+++ remove_element.js
@@ -0,0 +1,13 @@
+
+var removeElement = function(nums, val) {
+    let x=0
+    for (let a=0;a<nums.length;a++){
+        if(nums[a]!==val){
+        nums[x]= nums[a]
+        x++
+        }
+    }
+    return x
+
+}
+
